8216cc53be3bfa8c8d1aee2f976f57413cc85627
Tomáš Látka <latissek@gmail.com>
2024-06-30 19:15:52 +0200

    Expansion? Expansion!
    Added all of the material counters, logic is next!

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
 # Money counter for Mars: Terraformed
 import os
 
-income = 0
-bank = 0
 teraforming_rate = 20
+# Material counters
+bank = 0
+income = 0
+titan, metal, plants, energy, heat = 0, 0, 0, 0, 0
+titan_income, metal_income, plants_income, energy_income, heat_income = 0, 0, 0, 0, 0
 
 print("Welcome to Mars: Terraformed!")
-income = int(input("How much income do you have from pregame? "))
+# Ask the user for the pregame stats
+income = int(input("How much credit income did you get from pregame? "))
 bank = int(input("How much money did you get from pregame? "))
+metal_income = int(input("How much metal income did you get from pregame? "))
+metal = int(input("How much metal did you get from pregame? "))
+titan_income = int(input("How much titan income did you get from pregame? "))
+titan = int(input("How much titan did you get from pregame? "))
+plants_income = int(input("How much plants income did you get from pregame? "))
+plants = int(input("How much plants did you get from pregame? "))
+energy_income = int(input("How much energy income did you get from pregame? "))
+energy = int(input("How much energy did you get from pregame? "))
+heat_income = int(input("How much heat income did you get from pregame? "))
+heat = int(input("How much heat did you get from pregame? "))
+teraforming_rate += int(input("How much terraforming rate did you get from pregame? "))
+# Clear the screen
 os.system('clear')
+
+# Main game loop
 while True:
     print(f"Current income: {income + teraforming_rate}")
     print(f"Current bank: {bank}")
